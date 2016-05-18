2c05112768c323fc3a095c9aa5c834ccecf9bb9c
OguzhanRNaz <oguzhan_naz@hotmail.de>
2016-05-18 16:23:51 +0200

    Funktioniert

--- Motorsteuerung/main.cpp
+++ Motorsteuerung/main.cpp
@@ -3,21 +3,17 @@
 #include <iostream>
 #include <wiringPi.h>
 #include <softPwm.h>
-
 #include "steuerung.h"
-#include "USSensor.h"
 
 using namespace std;
 
 int main(void){
-	init_motorsteuerung(); //Init
-	setup(); //INit USS
-	while(getDistance()<=10cm){
-		rm_vor(100,0);
-		lm_vor(100,0);
-		//Abhängigkeit zu IRSensor1 und 2 fehlen noch
-		//Aber da könnte man einfach zwei weitere Bedingungen hinzufügen.
-		//Wenn IRSensor1(linkerSensor)==1(keine Linie) dann soll rechter Motor solange langsamer werden bis linker sensor wieder einen Wert 1 hat usw..
-	}
+	init_motorsteuerung();
+	cout << "Vor\n";
+	rm_vor(100,500);
+    lm_vor(100,5000);
+	cout << "Zur";
+	rm_zur(30,500);
+    lm_zur(30,5000);
 	return 0;
 }

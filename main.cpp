e34886bea615884fda8c6fcbf455465942d129e9
olibro <olibro@gmx.net>
2016-05-28 21:13:34 +0200

    Main fuer USS und Motorsteuerung
    Mainfunktion steuert in Abhängigkeit von USS die Motoren. Es funktioniert soweit gut, nur erkennt der USS nicht immer sofort ein Objekt und bei kleinen Geschwindigkeiten funktioniert ein Lastwechsel, d.h. von vorwärts in rückwärts Richtung, nicht immer.

---
+++ main.cpp
@@ -0,0 +1,47 @@
+
+//Compile: g++ -Wall -pedantic main.cpp steuerung.cpp -o steuerung -lwiringPi -lpthread
+
+/*
+	Zum Testen haben wir die Motoren an die folgende wiringPi Pins angeschlossen
+	LM_VOR	21
+	LM_ZUR	22
+	RM_VOR	23
+	RM_ZUR	24
+	
+	Ultraschallsensor
+	TRIG 7
+	ECHO 0
+*/
+
+#include <iostream>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+//Pfad muss an lokale Verzeichnisstruktur angepasst werden.
+#include "./USSensor/USSensor.h"
+#include "./Motorsteuerung/steuerung.h"
+
+using namespace std;
+
+int main(void){
+    if(wiringPiSetup()==-1)
+    {
+        cout << "Initalisation failed.\n";
+    }
+	init_motorsteuerung();
+	init_USSensor();
+	
+	//steuerung(int pwrRM, int pwrLM, int time);
+    while(1){
+		delay(100);		//Umschaltzeit. Zum Bremsen des Motors
+		while(getDistance()>=20){
+			steuerung(50, 50, 0);
+		}
+		delay(100);
+		while(getDistance()<20){
+			steuerung(-50,-50,0);
+		}
+		steuerung(0, 0, 0);
+	}	
+	return 0;
+}

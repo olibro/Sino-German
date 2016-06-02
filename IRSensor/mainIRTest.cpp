732ef53d279fe49a6d74dd1065ab47902f2ac008
aboutsun <sh_sunman@163.com>
2016-06-02 21:03:12 +0800

    Update mainIRTest.cpp

--- IRSensor/mainIRTest.cpp
+++ IRSensor/mainIRTest.cpp
@@ -12,18 +12,19 @@ int main(void)
     init_IRSensor();
     while(1)
     {
-    SR = digitalRead(SensorRight);      // SR=1→der rechte Infrarotsensor befindet sich innerhalb weisser zone;SR=0→ schwarze Linie
-    SL = digitalRead(SensorLeft);       // SL=1→der linke Infrarotsensor befindet sich innerhalb weisser zone;SL=0→ schwarze Linie
+    SR = digitalRead(SensorRight);      // SR=1→der rechte Infrarotsensor befindet sich innerhalb schwarzer zone;SR=0→ weisse Zone
+    SL = digitalRead(SensorLeft);       // SL=1→der linke Infrarotsensor befindet sich innerhalb schwarzer zone;SL=0→ weisseZone
     //steurung(int pwrRM, int pwrLM, int time);
-    //die schwarzen Linie ist dünner als die Abstand von zwei Sensoren
+    /*die schwarzen Linie ist dünner als die Abstand von zwei Sensoren
+    */
     if (SL == LOW&&SR==LOW)          // die zwei Infrarotsensor befinden sich in weisser Zone → das ist gut
-    steuerung(50,50,0);                         
-    else if (SL == LOW & SR == HIGH)    // rechter IRSensor ist innerhalb der scharzen Linie, das Auto braucht links zu fahren
-    steuerung(50,0,0);
-    else if (SR == LOW & SL == HIGH)    // linker IRSensor ist innerhalb der scharzen Linie, das Auto braucht rechts zu fahren
-    steuerung(0,50,0);
-    else                                // zwei Sensoren sind in weisser Zone→stopp
-    steuerung(0,0,0);
+    run();                         
+    else if (SL == LOW & SR == HIGH)    // rechter IRSensor ist innerhalb der schwarzen Linie und der linke Sensor ist in weisser Zone, das Auto braucht rechts zu fahren
+    right();
+    else if (SR == LOW & SL == HIGH)    // linker IRSensor ist innerhalb der scharzen Linie und der rechter Sensor ist in schwarzer Zone, das Auto braucht links zu fahren
+    left();
+    else                                // zwei Sensoren sind in weisser Zone → stopp
+    brake();
     }
     return 0;
        

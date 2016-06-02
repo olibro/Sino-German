34f22eb807b891b0e75b93cf21764d875c6af68d
aboutsun <sh_sunman@163.com>
2016-06-02 17:26:50 +0800

    Update mainIRTest.cpp

--- IRSensor/mainIRTest.cpp
+++ IRSensor/mainIRTest.cpp
@@ -1,35 +1,30 @@
 #include <wiringPi.h>
 #include <iostream>
 #include "IRSensor.h"
+#include <steuerung.h>
 
-
-#define SensorRight 4 //右循迹红外传感器(P3.2 OUT1)
-#define SensorLeft 5 //左循迹红外传感器(P3.3 OUT2)
 
 int main(void)
 {
-    int SL; //左循迹红外传感器状态
-    int SR; //右循迹红外传感器状态
+    int SL; //Zustand des linken IRSensor
+    int SR; //Zustand des rechten IRSensor
     wiringPiSetup();
-    setup();
-    keysacn();
+    init_IRSensor();
     while(1)
     {
-        // 有信号为LOW 没有信号为HIGH
-        SR = digitalRead(SensorRight);      // 有信号表明在白色区域，车子底板上L3亮；没信号表明压在黑
-        线上，车子底板上L3灭
-        SL = digitalRead(SensorLeft);       // 有信号表明在白色区域，车子底板上L2亮；没信号表明压在黑线
-        上，车子底板上L2灭
-        if (SL == LOW&&SR==LOW)
-            run();                          // 调用前进函数
-        else if (SL == HIGH & SR == LOW)    // 左循迹红外传感器,检测到信号，车子向右偏离轨道，向左
-            转
-            left();
-        else if (SR == HIGH & SL == LOW)    // 右循迹红外传感器,检测到信号，车子向左偏离轨道，向右
-            转
-            right();
-        else                                // 都是白色, 停止
-            brake();
+    SR = digitalRead(SensorRight);      // SR=1→der rechte Infrarotsensor befindet sich innerhalb weisser zone;SR=0→ schwarze Linie
+    SL = digitalRead(SensorLeft);       // SL=1→der linke Infrarotsensor befindet sich innerhalb weisser zone;SL=0→ schwarze Linie
+    //steurung(int pwrRM, int pwrLM, int time);
+    //
+    if (SL == LOW&&SR==LOW)          // die zwei Infrarotsensor befinden sich innerhalb der schwarzen Linie→das ist gut
+    steuerung(50,50,0);                         
+    else if (SL == LOW & SR == HIGH)    // rechter IRSensor ist in weisse Zone, das Auto braucht links zu fahren
+    steuerung(50,0,0);
+    else if (SR == LOW & SL == HIGH)    // linker IRSensor ist in weisse Zone, das Auto braucht rechts zu fahren
+    steuerung(0,50,0);
+    else                                // zwei Sensoren sind in weisser Zone→stopp
+    steuerung(0,0,0);
     }
     return 0;
+       
 }

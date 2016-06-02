00b4f567021d76827f800b51e2e319301f254df5
aboutsun <sh_sunman@163.com>
2016-06-02 16:44:40 +0800

    Initialisierung von IRSensor

--- IRSensor/IRSensor.h
+++ IRSensor/IRSensor.h
@@ -1,16 +1,6 @@
 #ifndef IRSENSOR_H
 #define IRSENSOR_H
 
-void setup();
-void run();
-void brake();
-void left();
-void spin_left(int time);
-void right();
-void spin_right(int time);
-void back(int time);
-void keysacn();
-
-
+void init_IRSensor();
 
 #endif // IRSENSOR_H_INCLUDED

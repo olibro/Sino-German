477c95fe4712de0d52fa916a6e6259dfffb7bf33
aboutsun <sh_sunman@163.com>
2016-06-02 17:30:17 +0800

    Update mainIRTest.cpp

--- IRSensor/mainIRTest.cpp
+++ IRSensor/mainIRTest.cpp
@@ -15,12 +15,12 @@ int main(void)
     SR = digitalRead(SensorRight);      // SR=1→der rechte Infrarotsensor befindet sich innerhalb weisser zone;SR=0→ schwarze Linie
     SL = digitalRead(SensorLeft);       // SL=1→der linke Infrarotsensor befindet sich innerhalb weisser zone;SL=0→ schwarze Linie
     //steurung(int pwrRM, int pwrLM, int time);
-    //
-    if (SL == LOW&&SR==LOW)          // die zwei Infrarotsensor befinden sich innerhalb der schwarzen Linie→das ist gut
+    //die scharzen Linie ist dünner als die Abstand von zwei Sensoren
+    if (SL == LOW&&SR==LOW)          // die zwei Infrarotsensor befinden sich in weisser Zone → das ist gut
     steuerung(50,50,0);                         
-    else if (SL == LOW & SR == HIGH)    // rechter IRSensor ist in weisse Zone, das Auto braucht links zu fahren
+    else if (SL == LOW & SR == HIGH)    // rechter IRSensor ist innerhalb der scharzen Linie, das Auto braucht links zu fahren
     steuerung(50,0,0);
-    else if (SR == LOW & SL == HIGH)    // linker IRSensor ist in weisse Zone, das Auto braucht rechts zu fahren
+    else if (SR == LOW & SL == HIGH)    // linker IRSensor ist innerhalb der scharzen Linie, das Auto braucht rechts zu fahren
     steuerung(0,50,0);
     else                                // zwei Sensoren sind in weisser Zone→stopp
     steuerung(0,0,0);

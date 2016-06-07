a1ec5c9e09b803f19845ec6695be73c26ba3656f
OguzhanRNaz <oguzhan_naz@hotmail.de>
2016-06-07 21:10:12 +0200

    fertig

--- mainforKListener.cpp
+++ mainforKListener.cpp
@@ -6,8 +6,8 @@
 #include "./USSensor/USSensor.h"
 #include "./Motorsteuerung/steuerung.h"
 
-#define PMIN 50;
-#define MMIN -50;
+#define PMIN 60;
+#define MMIN -60;
 
 
 using namespace std;
@@ -19,12 +19,15 @@ int main(int argc, char* argv[])
     int PRM, PLM, time;
     bool active = true;
 
+    int pmin = PMIN;
+    int mmin = MMIN;
+
     stringstream ss;
 
     // Parameter Einlesen - zu int Konvertieren - wert Variablen zuweisen
     ss << argv[1] << ' ' << argv[2] << ' ' << argv[3];
     ss >> cSpeed >> cDirection >> time;
-
+    cout <<argv[0] << argv[1] << argv[2] << argv[3];
     if(wiringPiSetup()==-1)
     {
         cout << "Initalisation failed.\n";
@@ -34,88 +37,100 @@ int main(int argc, char* argv[])
     init_USSensor();
     // init_irsens();
 
-    cout << "Starte Motor" << endl << "Distance = " << getDistance() << endl;
+    cout << "Erhaltene Parameter =  " << cSpeed << ", " << cDirection << ", " << time << endl;
+    cout << "Distance = " << getDistance() << endl;
 
-    while(getDistance()>=8 && active){
+    while(getDistance()>=8 && active)
+    {
 
-    // Vorwärts
-    if(cSpeed > 0)
-    {
-        //Rechts
-        if(cDirection > 0){
-            PRM = PMIN + ((cSpeed*10)/cDirection);
-            PLM = PMIN + (cSpeed*10);
+        // Vorwärts
+        if(cSpeed > 0)
+        {
+            //Rechts
+            if(cDirection > 0)
+            {
+                PRM = pmin + ((cSpeed*10)/cDirection);
+                PLM = pmin + (cSpeed*10);
+            }
+            //Links
+            else if(cDirection < 0)
+            {
+                PRM = pmin + (cSpeed*10);
+                PLM = pmin + ((cSpeed*10)/cDirection);
+            }
+            //gerade
+            else
+            {
+                PRM = pmin + (cSpeed*10);
+                PLM = pmin + (cSpeed*10);
+            }
         }
-        //Links
-        else if(cDirection < 0){
-            PRM = PMIN + (cSpeed*10);
-            PLM = PMIN + ((cSpeed*10)/cDirection);
+        // Rückwärt
+        else if(cSpeed <0)
+        {
+            //Rechts
+            if(cDirection > 0)
+            {
+                PRM = mmin + ((cSpeed*10)/cDirection);
+                PLM = mmin + (cSpeed*10);
+            }
+            //Links
+            else if(cDirection < 0)
+            {
+                PRM = mmin + (cSpeed*10);
+                PLM = mmin + ((cSpeed*10)/cDirection);
+            }
+            //gerade
+            else
+            {
+                PRM = mmin + (cSpeed*10);
+                PLM = mmin + (cSpeed*10);
+            }
         }
-        //gerade
-        else{
-            PRM = PMIN + (cSpeed*10);
-            PLM = PMIN + (cSpeed*10);
+        else // cspeed == 0
+        {
+            //Rechts
+            if(cDirection > 0)
+            {
+                PRM = 0;
+                PLM = mmin + (cDirection*10);
+            }
+            //Links
+            else if(cDirection < 0)
+            {
+                PRM = mmin + (cDirection*10);
+                PRM = mmin + (cDirection*10);
+                PLM = 0;
+            }
+            //gerade
+            else
+            {
+                PRM = 0;
+                PLM = 0;
+            }
         }
-    }
-    // Rückwärt
-    else if(cSpeed <0)
-    {
-        //Rechts
-        if(cDirection > 0){
-            PRM = MMIN + ((cSpeed*10)/cDirection);
-            PLM = MMIN + (cSpeed*10);
-        }
-        //Links
-        else if(cDirection < 0){
-            PRM = MMIN + (cSpeed*10);
-            PLM = MMIN + ((cSpeed*10)/cDirection);
-        }
-        //gerade
-        else{
-            PRM = MMIN + (cSpeed*10);
-            PLM = MMIN + (cSpeed*10);
-        }
-    }
-    else // cspeed == 0
-    {
-        //Rechts
-        if(cDirection > 0){
-            PRM = 0;
-            PLM = MMIN + (cDirection*10);
-        }
-        //Links
-        else if(cDirection < 0){
-            PRM = MMIN + (cDirection*10);
-            PRM = MMIN + (cDirection*10);
-            PLM = 0;
-        }
-        //gerade
-        else{
-            PRM = 0;
-            PLM = 0;
-        }
-    }
-            cout << "Motoren starten mit " << *PRM << ", " << *PLM << ", " << *time << endl;
-            steuerung(PRM, PLM, time);
-            delay(5000);
-            active = false;
-            steuerung(PRM, PLM, 1);
-    }
+        cout << "Motoren starten mit " << PRM << ", " << PLM << ", " << time << endl;
+        steuerung(PRM, PLM, time);
+        delay(1000);
+        active = false;
+        steuerung(PRM, PLM, 1);
 
+    }
+    return 0;
 }
 
 /*
             if(cSpeed == 0){ PRM == 0};
 
-            if(cSpeed == 1){ PRM = PMIN +10; PLM = PMIN +10}
-            if(cSpeed == 2){ PRM = PMIN +20; PLM = PMIN +20};
-            if(cSpeed == 3){ PRM = PMIN +30; PLM = PMIN +30};
-            if(cSpeed == 4){ PRM = PMIN +40; PLM = PMIN +40};
+            if(cSpeed == 1){ PRM = pmin +10; PLM = pmin +10}
+            if(cSpeed == 2){ PRM = pmin +20; PLM = pmin +20};
+            if(cSpeed == 3){ PRM = pmin +30; PLM = pmin +30};
+            if(cSpeed == 4){ PRM = pmin +40; PLM = pmin +40};
 
-            if(cSpeed == -1){ PRM = MMIN -10; PLM = MMIN +10};
-            if(cSpeed == -2){ PRM = MMIN -20; PLM = MMIN +20};
-            if(cSpeed == -3){ PRM = MMIN -30; PLM = MMIN +30};
-            if(cSpeed == -4){ PRM = MMIN -40; PLM = MMIN +40};
+            if(cSpeed == -1){ PRM = mmin -10; PLM = mmin +10};
+            if(cSpeed == -2){ PRM = mmin -20; PLM = mmin +20};
+            if(cSpeed == -3){ PRM = mmin -30; PLM = mmin +30};
+            if(cSpeed == -4){ PRM = mmin -40; PLM = mmin +40};
 
             if(cDirection == 0){};
             if(cDirection == 1){ PRM = PRM ; PLM = PLM +10};

af94da90dc8346cb9f31c2fb6945e445dcaf73fc
olibro <olibro@gmx.net>
2016-07-01 19:14:56 +0200

    Delete compileAndRun.sh

--- IRSensor/compileAndRun.sh
+++
@@ -1,8 +0,0 @@
-#!/etc/bash
-
-echo "Beginne die Datei mainIRTest.cpp und die Datei IRSensor.cpp zu compilern"
-g++ -Wall -pedantic mainIRTest.cpp IRSensor.cpp -o IRSensorTest -lwiringPi -lpthread
-echo "mainIRTest.cpp Compiliert -> Ausgabedatei = IRSensorTest"
-echo "Starte nun IRSensorTest"
-./IRSensorTest
-

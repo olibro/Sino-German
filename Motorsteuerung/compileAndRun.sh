1ad7fcd11c86e9d161b1565dae87c1cb585bb08c
olibro <olibro@gmx.net>
2016-07-01 19:14:05 +0200

    Delete compileAndRun.sh

--- Motorsteuerung/compileAndRun.sh
+++
@@ -1,7 +0,0 @@
-#!/etc/bash
-
-echo "Beginne die Datei main.cpp und die Datei steuerung.cpp zu compilern"
-g++ -Wall -pedantic main.cpp steuerung.cpp -o Steuerung -lwiringPi -lpthread
-echo "main.cpp Compiliert -> Ausgabedatei = Steuerung"
-echo "Starte nun Steuerung"
-./Steuerung

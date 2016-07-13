fcd21c11636c2373a09644e2b4aa25df548e580b
Matt-Ze <matthias.sebald@gmail.com>
2016-07-13 16:24:05 +0200

    Add files via upload
    Doxygen Kommentar hinzugefuegt

--- USSensor/USSensor.cpp
+++ USSensor/USSensor.cpp
@@ -8,7 +8,16 @@ using namespace std;
 #define TRIG 7
 #define ECHO 0
 
-//Pin zuweisung
+/// Pin zuweisung US Sensor
+/**
+	\param TRIG Pin als Ausgang
+	\param ECHO Pin als Eingang
+	Echo und Trigger Pin am Raspberry Pi werden ueber die Funktion pinMode () aus der Headerdatei wiringPi.h deklariert
+	HIGH bzw. LOW sind ebenfalls in der Headerdatei wiringPi.h deklariert
+	LOW entspricht 0
+	HIGH entspricht 1
+*/
+
 void init_USSensor()
 {
     pinMode(TRIG, OUTPUT);
@@ -18,6 +27,21 @@ void init_USSensor()
     digitalWrite(TRIG, LOW);
     delay(30);
 }
+
+
+/// Berechnen der Distanz zum nächsten Objekt
+/**
+	digitalWrite und delayMicroseconds sind in der Headerdatei wiringPi.h deklariert
+	Vorgang zum ermitteln der Distanz:
+	1. TRIG wird für 20ms eingeschaltet und danach wieder ausgeschaltet, dies dient zum aussenden der Schallwellen
+	2. warte darauf, das ECHO LOW ist
+	3. starte einen Timer der Microsekunden zaehlt
+	4. solange bis ECHO HIGH ist.
+	5. Auswerten des Timers 
+	\param distance wird die Distanz in Zentimetern (cm) zugewiesen
+	Der Rückgabewert der Funktion ist die Entfernung zum nächsten Objekt in cm.
+*/
+
 int getDistance()
 {
     digitalWrite(TRIG, HIGH);

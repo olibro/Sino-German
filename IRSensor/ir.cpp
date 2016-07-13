fffe5c87be268c31e778c479bf1253edc985a8b1
Matt-Ze <matthias.sebald@gmail.com>
2016-07-13 16:23:28 +0200

    Add files via upload
    Doxygen Kommentar hinzugefuegt

--- IRSensor/ir.cpp
+++ IRSensor/ir.cpp
@@ -4,6 +4,15 @@
 #define IRL 5 //SENSORLEFT PHYSICAL=18
 #define IRR 4 //SENSORRIGHT PHYSICAL=16
 
+
+/// Pin zuweisung IR Sensor
+/**
+	\param IRL linker Infrarotsensor
+	\param IRR rechter Infrarotsensor
+	rechter und linker Infrarotsensor am Pin x des Raspberry Pi werden ueber die Funktion pinMode () aus der Headerdatei wiringPi.h deklariert
+	HIGH bzw. LOW sind ebenfalls in der Headerdatei wiringPi.h deklariert
+*/
+
 void init_irsens(void){
 	pinMode(IRR,INPUT);
 	pinMode(IRL,INPUT);
